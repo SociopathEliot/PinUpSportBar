4d05f20d35e44a89f04978b83bb541e8c1004065
DanielMamytov <mamytovdaniel.08.02@gmail.com>
2025-06-22 05:15:58 +0600

    Remove invalid blur methods

--- app/src/main/java/com/pinup/barapp/ui/MainActivity.kt
+++ app/src/main/java/com/pinup/barapp/ui/MainActivity.kt
@@ -1,17 +1,36 @@
 package com.pinup.barapp.ui
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.pinup.barapp.R
+import eightbitlab.com.blurview.BlurView
+import eightbitlab.com.blurview.RenderScriptBlur
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var blurView: BlurView
+
+    fun showBlur() {
+        blurView.visibility = android.view.View.VISIBLE
+    }
+
+    fun hideBlur() {
+        blurView.visibility = android.view.View.GONE
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        blurView = findViewById(R.id.blurView)
+        val rootView = findViewById<ViewGroup>(android.R.id.content)
+        val windowBackground: Drawable = window.decorView.background
+        blurView.setupWith(rootView, RenderScriptBlur(this))
+            .setFrameClearDrawable(windowBackground)
+            .setBlurRadius(16f)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/pinup/barapp/ui/fragments/HelpDialogFragment.kt
+++ app/src/main/java/com/pinup/barapp/ui/fragments/HelpDialogFragment.kt
@@ -1,7 +1,34 @@
+package com.pinup.barapp.ui.fragments
+
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.pinup.barapp.databinding.DialogHelpBinding
+import com.pinup.barapp.ui.MainActivity
 
 class HelpDialogFragment : DialogFragment() {
     private var _binding: DialogHelpBinding? = null
     private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DialogHelpBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
+        (activity as? MainActivity)?.hideBlur()
+    }
 }
