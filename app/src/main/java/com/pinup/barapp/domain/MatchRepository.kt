97fb35124f7fc0f532c6a3fd0293bb40e237a2ef
SociopathEliot <dlaonlcahik@gmail.com>
2025-06-24 15:18:37 +0600

    Add month filtering for schedule

--- app/src/main/java/com/pinup/barapp/domain/MatchRepository.kt
+++ app/src/main/java/com/pinup/barapp/domain/MatchRepository.kt
@@ -1,7 +1,9 @@
 package com.pinup.barapp.domain
 
 import com.pinup.barapp.domain.models.Match
+import java.time.Month
 
 interface MatchRepository {
     suspend fun getUpcomingMatches(): List<Match>
+    suspend fun getMatchesByMonth(month: Month): List<Match>
 }
